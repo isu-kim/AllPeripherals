a368d6c3d2f45450af0342fad9a617c704c3d1f3
Isu Kim <49092508+gooday2die@users.noreply.github.com>
2020-04-26 12:13:17 +0900

    Added Runner bat and Updated Handle Script
    Added runnner batch files for user's ease of executing this program. Also, added a parameter to main.py for speed.

---
+++ WindowsSupport/ForGitHubRelease/x64/pythonSide/Bridge.py
@@ -0,0 +1,67 @@
+from socket import *
+
+
+class Bridge():
+    """
+    A class that manages all connections to server
+    """
+    def __init__(self, mode, port=37716):
+        """
+        An intializer method for class Bridge
+        @params {int} Operation mode to send to server
+        @params {int} Port number to send communications through. 37716 Default
+        """
+        self.operation_mode = mode
+        self.port = port
+        self.client_sock = socket(AF_INET, SOCK_STREAM)
+
+        self.connect_server()
+        self.send_operation_mode()
+
+    def connect_server(self):
+        """
+        A method that connects to server for the first time.
+        @params {void}
+        @returns {void}
+        """
+        print("[Client] Connecting to port : " + str(self.port))
+
+        try:
+            self.client_sock.connect(('127.0.0.1', self.port))
+
+        except ConnectionRefusedError:  # the case when server is closed
+            print("[ERROR] Cannot connect to server.")
+            print("[ERROR] Please check if server engine is running")
+            exit(0)
+
+    def send_operation_mode(self):
+        """
+        A method that sends server which operation mode would be used.
+        @params {void}
+        @returns {void}
+        """
+        print("[Client] Sent operation mode : " + str(self.operation_mode))
+
+        operation_mode = bytearray()
+        operation_mode.append(self.operation_mode)
+        self.client_sock.send(operation_mode)
+
+    def send_rgb_value(self, r, g, b):
+        """
+        A method that sends server RGB value.
+        @params {int} Red value, 0 ~ 255
+        @params {int} Green value, 0 ~ 255
+        @params {int} Blue value, 0 ~ 255
+        @returns {void}
+        """
+        send_byte = bytearray()
+        send_byte.append(r)
+        send_byte.append(g)
+        send_byte.append(b)
+
+        try:
+            self.client_sock.send(send_byte)
+
+        except BrokenPipeError:  # the case when server is closed.
+            print("[Server] The Server is closed!")
+            exit(0)
---
+++ WindowsSupport/ForGitHubRelease/x86/ScreenReactive.bat
@@ -0,0 +1,2 @@
+@start "" /B %SystemRoot%\System32\cmd.exe /C %cd%/cppSide/AllPheriperalCPP.exe
+@start "" /B %SystemRoot%\System32\cmd.exe /C python %cd%/pythonSide/main.py -easteregg false -operationmode 1
